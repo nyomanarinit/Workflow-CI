62903c4d7047096edac6c4d64d0e44f852201c6b
Nyoman Arini Trirahayu <145880551+nyomanarinit@users.noreply.github.com>
2025-12-20 13:54:50 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import argparse
+import mlflow
+import mlflow.sklearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
-import mlflow
-import mlflow.sklearn
+from sklearn.pipeline import Pipeline
 
 DATA_PATH = "churn_preprocessed.csv"
 EXPERIMENT_NAME = "Customer Churn Prediction"
 
 def main(args):
-    print("🚀 Training dimulai...")
-
     mlflow.set_experiment(EXPERIMENT_NAME)
-    mlflow.sklearn.autolog(log_models=False)
+    mlflow.sklearn.autolog()
 
     df = pd.read_csv(DATA_PATH)
+
     X = df.drop("Exited", axis=1)
     y = df["Exited"]
 
@@ -23,12 +24,9 @@ def main(args):
         X, y, test_size=0.2, stratify=y, random_state=42
     )
 
-    scaler = MinMaxScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    with mlflow.start_run() as run:
-        model = RandomForestClassifier(
+    pipeline = Pipeline([
+        ("scaler", MinMaxScaler()),
+        ("model", RandomForestClassifier(
             n_estimators=args.n_estimators,
             min_samples_split=args.min_samples_split,
             min_samples_leaf=args.min_samples_leaf,
@@ -36,35 +34,29 @@ def main(args):
             max_depth=args.max_depth,
             bootstrap=args.bootstrap,
             random_state=42
-        )
+        ))
+    ])
 
-        model.fit(X_train, y_train)
-
-        acc = model.score(X_test, y_test)
+    with mlflow.start_run():
+        pipeline.fit(X_train, y_train)
+        acc = pipeline.score(X_test, y_test)
         mlflow.log_metric("accuracy", acc)
 
-        # WAJIB supaya workflow bisa build docker
         mlflow.sklearn.log_model(
-            model,
+            pipeline,
             artifact_path="model"
         )
 
-        run_id = run.info.run_id
-        with open("run_id.txt", "w") as f:
-            f.write(run_id)
-
         print(f"✅ Training selesai | Accuracy: {acc}")
-        print(f"🆔 Run ID: {run_id}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
     parser.add_argument("--n_estimators", type=int, default=500)
     parser.add_argument("--min_samples_split", type=int, default=5)
     parser.add_argument("--min_samples_leaf", type=int, default=4)
     parser.add_argument("--max_features", type=float, default=0.5)
     parser.add_argument("--max_depth", type=int, default=15)
     parser.add_argument("--bootstrap", type=bool, default=True)
-
     args = parser.parse_args()
+
     main(args)
